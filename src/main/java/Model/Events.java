9c97161b719eed4702a9606a3e876dce79f3de85
einemint <einemint@gmail.com>
2022-03-13 12:05:21 +0300

    Add Hibernate

--- src/main/java/Model/Events.java
+++ src/main/java/Model/Events.java
@@ -1,4 +1,92 @@
 package Model;
 
+import jakarta.persistence.*;
+
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "events")
 public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+
+    @Column
+    @NotNull
+    public String type;
+
+    @Column
+    @NotNull
+    public String title;
+
+    @Column
+    @NotNull
+    public String date;
+
+    @Column
+    public String location;
+
+    @Column
+    @NotNull
+    public Boolean online;
+
+    @Column
+    @NotNull
+    public String link;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Boolean getOnline() {
+        return online;
+    }
+
+    public void setOnline(Boolean online) {
+        this.online = online;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
---
+++ src/main/java/Model/HibernateConnection.java
@@ -0,0 +1,24 @@
+package Model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateConnection {
+    public static final String HIBERNATE_CONFIG_FILE = "hibernate.cfg.xml";
+    private static SessionFactory sessionFactory = getSessionFactory();
+
+    private static SessionFactory getSessionFactory() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure(HIBERNATE_CONFIG_FILE).build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+
+        return metadata.getSessionFactoryBuilder().build();
+    }
+
+    private static synchronized Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
